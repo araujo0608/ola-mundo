54037fc341dea852bb7d263c23b19d0c8293473f
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2022-02-23 12:42:52 -0300

    Codigo base

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print('Olá Mundo em Python!')
+x = float(input('Digite um numero: '))
+y = float(input('Digite outro numero: '))
+
+print('\n *** ESCOLHA UMA OPÇÃO ***\n')
+print('1 - Somar')
+print('2 - Subtrair')
+print('3 - Multiplicar')
+print('4 - Dividir')
+option = int(input(': '))
