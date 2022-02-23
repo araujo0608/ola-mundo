888adb0bb496f891fe9096002b57bb5ef972a04f
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2022-02-23 12:53:57 -0300

    Funcoes criadas

---
+++ functions.py
@@ -0,0 +1,8 @@
+def somar(x, y):
+    return x + y
+def subtrair(x, y):
+    return x - y
+def mult(x, y):
+    return x * y
+def div(x, y):
+    return x / y
--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import functions as func
+
 x = float(input('Digite um numero: '))
 y = float(input('Digite outro numero: '))
 
@@ -7,3 +9,12 @@ print('2 - Subtrair')
 print('3 - Multiplicar')
 print('4 - Dividir')
 option = int(input(': '))
+
+if option == 1:
+    print(func.somar(x, y))
+elif option == 2:
+    print(func.subtrair(x, y))
+elif option == 3:
+    print(func.mult(x, y))
+else:
+    print(func.div(x, y))
